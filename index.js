6754a455dafa0326560cfe7dc1449788d034e8e4
hamzaboulman <hamzaboulman132@gmail.com>
2022-03-24 12:11:23 +0100

    fix bugg

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var express = require('express');  
-var app     = express(); 
- 
-app.get('/', function(req, res) {  
-    var url = 'http://universities.hipolabs.com/search?country=morocco';
+var fullUrl = 'http://universities.hipolabs.com/search?country=morocco';
+var express = require('express');
+var app = express();
+
+app.get('/country', function(req, res) {
+    app.use((req, res, next) => {
+        const path = url.parse(req.url).path;
+        console.log(path)
+    });
 
 });
 
-app.listen(2020, () =>{
+// app.use((req, res, next) => {
+//     const path = url.parse(req.url).path;
+//     console.log(path)
+// });
+
+app.listen(2020, () => {
     console.log('serveur')
 })
-

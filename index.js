a510c197c47f08bf49a3686781322161f74b6ef1
asmae <imazziasmae@gmail.com>
2022-03-24 11:59:46 +0100

    get country

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var express = require('express');  
-var app     = express(); 
- 
-app.get('/', function(req, res) {  
-    var url = 'http://universities.hipolabs.com/search?country=morocco';
+const express = require("express"); 
+const app = express();
+const axios = require('axios') 
 
+app.get("/", (req, res) => {
+    axios.get('http://universities.hipolabs.com/search?country=morocco')
+    .then(response=>{
+      // handle success
+      console.log(response)
+      res.send(response.data)
+    })
 });
 
 app.listen(2020, () =>{
